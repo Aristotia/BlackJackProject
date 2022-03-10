d9ab85479ceb9e73dbffdafbe9c229b4889e5c06
saif-eddine-miah <saifeddinemiah@gmail.com>
2022-03-10 12:16:15 +0100

    added emoji's and space

--- script.js
+++ script.js
@@ -77,16 +77,25 @@ while(gameOver){
   
 gameOver = confirm("Voulez-vous commencer une nouvelle partie?");
 
+//VERSION 2
+console.log("♠️ ♣️ Bienvenue au BlackJack! ♥️ ♦️")
+//  *Le croupier donne deux valeurs au joueur
+console.log(``)
+console.log("♠️ ♣️ VOTRE MAIN ♥️ ♦️")
 let playerTotal = 0;
+let randomValue = 0;
+for(let i=0 ; i < 2 ; i++){
+  randomValue = Math.floor(Math.random() * 10) + 1;
+  console.log(`Vous avez tiré le..... ${randomValue}!!`);
+  playerTotal += randomValue;
+}
 
-let playerValue1 = Math.floor(Math.random() * 10) + 1;
-let playerValue2 = Math.floor(Math.random() * 10) + 1;
-playerTotal = playerValue1 + playerValue2;
+console.log(`Votre total est de ${playerTotal}.`);
 
-console.log (`Tes cartes sont le ${playerValue1} et le ${playerValue2}. 
-Ton total vaut ${playerTotal}.`)
 
 //le croupier donne une valeur visible et une valeur cachée
+console.log(``)
+console.log("♠️ ♣️ LA BANQUE ♥️ ♦️")
 
 let dealerTotal = 0;
 
@@ -94,34 +103,34 @@ let dealerValue1 = Math.floor(Math.random() * 10) + 1;
 let hiddenDealerValue = Math.floor(Math.random() * 10) + 1;
 dealerTotal = dealerValue1 + hiddenDealerValue;
 
-console.log (`La carte du dealer vaut ${dealerValue1}.`)
+console.log (`Le croupier a tiré le.....${dealerValue1} et une carte secrete.`)
 
 //Tour du joueur
-
+console.log(``)
 if (playerTotal < 21) {
-    while (confirm("Veux tu tirer une nouvelle carte?")) {
+    while (confirm("Voulez-vous tirer une nouvelle carte?")) {
       let playerValue3 = Math.floor(Math.random() * 10) + 1;
-  
+  console.log(``)
       playerTotal += playerValue3;
-      console.log(`Ta nouvelle carte est le ${playerValue3}. Ton total est de ${playerTotal}.`)
+      console.log(`Votre nouvelle carte est le ${playerValue3}.Votre total est de ${playerTotal}.`)
       if (playerTotal >= 21) {
-        break ;
+        break;
       }
     }
   }
   
   if (playerTotal === 21) {
-    console.log("Blackjack!!! Bravo tu a gagné!")
-    continue
+    console.log("Blackjack!!! Bravo vous avez gagné!")
+    return
   }
   if (playerTotal > 21) {
-    console.log("Tu as perdu =(")
-    continue
+    console.log("Votre main est perdante =(.")
+    return
   }
 
 //Tour du dealer
 
-  console.log("C'est le tour du dealer.")
+  console.log("C'est le tour du croupier.")
   
   if (dealerTotal < 17){
   let dealerValue3 = Math.floor(Math.random() * 10) + 1;
@@ -130,13 +139,13 @@ if (playerTotal < 21) {
 }
 
 if (dealerTotal===21){
-    console.log("Blackjack!!! Tu as perdu =(")
-  continue
+    console.log("Blackjack!!! Vous avez perdu =(")
+  return
 }
 
 if (dealerTotal>21){
-    console.log("Tu as gagné =)")
-  continue
+    console.log("Vous avez gagné =)")
+  return
   };
 
 //Comparatif des totaux et déclaration du vainqueur
